83914ee1f0c849c5ccdf854708ccb62073692df4
Carlos Daniel Drury <carlosdanieldrury@hotmail.com>
2017-08-17 00:34:40 -0300

    Added items to the view - activity_main.xml - MainActivity.java

--- app/src/main/java/db/drury/com/dbchallenge/MainActivity.java
+++ app/src/main/java/db/drury/com/dbchallenge/MainActivity.java
@@ -2,28 +2,53 @@ package db.drury.com.dbchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Objects
+
+    private BootstrapButton buttonGetKey;
+    private BootstrapButton buttonDeleteData;
+    private TextView textViewTokenInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void initialize() {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+        // object references
+        buttonGetKey = (BootstrapButton) findViewById(R.id.buttonGetKey);
+        buttonDeleteData = (BootstrapButton) findViewById(R.id.buttonDeleteData);
+
+        // onClickListeners
+        View.OnClickListener buttonGetKeyListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+
+            }
+        };
+
+        View.OnClickListener buttonDeleteDataListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+
+            }
+        };
+
+        buttonGetKey.setOnClickListener(buttonGetKeyListener);
+        buttonDeleteData.setOnClickListener(buttonDeleteDataListener);
     }
+
 }
